1add0bb4bbe828c76196d6b7a6afa9bee4784567
palak <palakgupta120898@gmail.com>
2022-03-11 15:54:15 +0530

    0,0 is adding is done

--- calculator_test.go
+++ calculator_test.go
@@ -32,3 +32,18 @@ func test_add_2_0(t *testing.T) {
 		t.Errorf("Add(%d,%d) = %d, wanted %d", a, b, got, wanted)
 	}
 }
+func test_add_0_0(t *testing.T) {
+	t.Logf("hello")
+	// define variables for test / Preparation
+	a := 0
+	b := 0
+	wanted := 0
+
+	// call function to test
+	got := Add(a, b)
+
+	// verify result
+	if got != wanted {
+		t.Errorf("Add(%d,%d) = %d, wanted %d", a, b, got, wanted)
+	}
+}
